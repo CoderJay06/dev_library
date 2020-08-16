0805c4e79c7ed3b1fc67db50de92d647a19bd9f1
CoderJay06 <jaycru06@yahoo.com>
2020-08-16 00:54:48 -0400

    Update users controller create and show actions, add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
 
    # signup user
    def create 
-      raise params.inspect
+      # create new user 
+      @user = User.create(user_params)
+      # check if user was created 
+      if @user.save
+         # login and redirect to user show
+         session[:user_id] = @user.id
+         redirect_to user_path(@user)
+      end 
+   end 
+
+   def show 
+      # binding.pry
+      @user = User.find_by_id(params[:id])
+   end 
+
+   private 
+   def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
    end 
 end
