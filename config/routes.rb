df8a05dcba3c018c95bb0487d0782d70a641d150
CoderJay06 <jaycru06@yahoo.com>
2020-08-17 02:38:14 -0400

    Refactor sessions and user signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/home'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/home', to: 'sessions#home'
   resources :subscriptions
   resources :reviews
   resources :categories
