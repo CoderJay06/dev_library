e472525ccba068cc810e5c999853775fc3844ff3
CoderJay06 <jaycru06@yahoo.com>
2020-08-17 03:23:44 -0400

    Add login form with create action,  fix invalid authenticity_token error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,25 @@
 class SessionsController < ApplicationController
+  # get login form
   def new
   end
 
+  # login user
   def create
+    # binding.pry
+    # find user
+    @user = User.find_by(username: params[:username])
+
+    # authenticate user data and log them in
+    if @user && @user.authenticate(params[:password])
+      # log them in and redirect them to their profile page
+      session[:user_id] = @user.id 
+      # binding.pry
+      redirect_to user_path(@user)
+    else 
+
+      # otherwise redirect them back to login
+      redirect_to login_path
+    end 
   end
 
   def home
