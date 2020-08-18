d60ab5af1b7d123eaf6e077dbc4bd0a09d3e4c31
CoderJay06 <jaycru06@yahoo.com>
2020-08-18 15:43:58 -0400

    Add select category option to new book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,6 @@ class BooksController < ApplicationController
 
    private
    def book_params
-      params.require(:book).permit(:title, :author, :description, :release_date)
+      params.require(:book).permit(:title, :author, :description, :release_date, :category)
    end 
 end
