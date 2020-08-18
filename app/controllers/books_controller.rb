c20798a7fa6d37aa60718788d585db8fe66f0593
CoderJay06 <jaycru06@yahoo.com>
2020-08-18 15:14:40 -0400

    Add form to create new books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,18 @@ class BooksController < ApplicationController
    def index 
       @books = Book.all 
    end 
+
+   def new 
+      @book = Book.new
+   end 
+
+   def create
+      @book = Book.create(book_params)
+      binding.pry
+   end 
+
+   private
+   def book_params
+      params.require(:book).permit(:title, :author, :description, :release_date)
+   end 
 end
