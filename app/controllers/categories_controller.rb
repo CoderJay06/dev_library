4647e824b717e949088317b0c720315f48b2f34a
CoderJay06 <jaycru06@yahoo.com>
2020-08-18 18:09:42 -0400

    Add new category form and create action, include category validations

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+   # render form for creating a category
+   def new
+      @category = Category.new 
+   end 
+
+   # create a new category
+   def create 
+      @category = Category.create(category_params)
+      if @category.save
+         # if category created successfully, send to homepage
+         redirect_to root_url
+      else  
+         # display errors and render create category form again
+         render :new 
+      end 
+   end 
+
+   private 
+   def category_params
+      params.require(:category).permit(:name)
+   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
    has_many :books
+   validates :name, presence: true, uniqueness: true
 end
