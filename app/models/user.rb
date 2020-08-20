4c4f9ee840447811be6957a793ff59b896d3d112
CoderJay06 <jaycru06@yahoo.com>
2020-08-20 12:45:30 -0400

    Include book show page links in books index, add email and password confirmation with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :subscriptions
   has_many :books, through: :subscriptions
-  validates :first_name, :last_name, :email, :username, :password,
-    presence: true
+  validates :first_name, :last_name, :email, :email_confirmation, 
+  :username, :password, :password_confirmation, presence: true
   validates :username, :email, uniqueness: true 
+  validates :email, :password_confirmation, confirmation: true
 end
