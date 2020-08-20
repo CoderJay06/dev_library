1e377e7d9b093b763c1872c7aaff2e89df7e920a
CoderJay06 <jaycru06@yahoo.com>
2020-08-20 12:57:42 -0400

    Include name length validations, display validation errors for user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :books, through: :subscriptions
   validates :first_name, :last_name, :email, :email_confirmation, 
   :username, :password, :password_confirmation, presence: true
+  validates :first_name, :last_name, length: {minimum: 2}
   validates :username, :email, uniqueness: true 
   validates :email, :password_confirmation, confirmation: true
 end
