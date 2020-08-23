72f1a9dd28dce972467eb6983a06f9d7d4278064
CoderJay06 <jaycru06@yahoo.com>
2020-08-23 04:30:53 -0400

    Include validation errors with edit book form and add edit button to book show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root 'sessions#home'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
   get '/home', to: 'sessions#home'
   get '/books/recently_added', to: 'books#recently_added'
   get '/books/:id/reviews/new', to: 'reviews#new'
   get '/books/:id/reviews/:id/edit', to: 'reviews#edit', as: 'edit_review'
+  post '/login', to: 'sessions#create'
   post '/books/:id/reviews/new', to: 'reviews#create'
   patch '/books/:id/reviews/:id/edit', to: 'reviews#update'
   delete '/books/:id/reviews/:id', to: 'reviews#destroy', as: 'delete_review'
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :books do 
     resources :reviews 
   end
+  resources :books 
   resources :subscriptions
   resources :categories
   resources :users
