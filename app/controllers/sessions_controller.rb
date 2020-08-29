20b999afc15370c305c35f9421acc27fd82a4fa1
CoderJay06 <jaycru06@yahoo.com>
2020-08-29 02:51:28 -0400

    Fix google omniauth method for signing in and creating users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,30 +30,31 @@ class SessionsController < ApplicationController
   end 
 
   def googleAuth # log users in with google omniauth
-    # get access tokens from google server
-    # binding.pry
-    user = User.find_or_create_by(email: auth['info']['email']) do |user|
-        random_password = SecureRandom.hex(16);
-        user.first_name = auth["info"]["name"].split(' ')[0]
-        user.last_name = auth["info"]["name"].split(' ')[1]
-        user.email = auth["info"]["email"]
-        user.email_confirmation = auth["info"]["email"]
-        user.username = auth["info"]["email"]
-        user.password = random_password
-        user.password_confirmation = random_password
-    end 
-    # binding.pry 
-
-    if user.valid?
-      # if user created successfully log them in and 
-      # send them to their profile
-      session[:user_id] = user.id 
-      redirect_to user_path
-    else
-      # otherwise, display error messages and 
-      # redirect them back to homepage
-      flash[:alert] = user.errors.full_messages
-      redirect_to root_path
+    if google_auth_hash = auth 
+      omniauth_email = auth["info"]["email"]
+      if user = User.find_by(email: omniauth_email)
+        # if user exists, log them in and send to profile page
+        session[:user_id] = user.id 
+        redirect_to user_path(user)
+      else   
+        # otherwise create new user
+        user = User.new(
+        first_name: auth["info"]["name"].split(' ')[0],
+        last_name: auth["info"]["name"].split(' ')[1],
+        email: auth["info"]["email"],
+        username: auth["info"]["email"],
+        password: SecureRandom.hex(16)
+        )
+        if user.save 
+          # if user created successfully then sign 
+          # them in and send to their profile page
+          session[:user_id] = user.id 
+          redirect_to user_path(user)
+        else  
+          # otherwise display errors
+          raise user.errors.full_messages.inspect 
+        end 
+      end 
     end 
   end 
 
