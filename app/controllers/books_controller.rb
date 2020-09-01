204f1c8e139956f036d5b6cbf510b605dd8231c1
github username <github email address>
2020-09-01 13:50:45 -0400

    Refactor books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,38 +12,33 @@ class BooksController < ApplicationController
    def create
       @book = Book.create(book_params)
       if @book.save
-         # if book created successfully, send to book show page
          redirect_to book_path(@book)
       else  
-         # otherwise render create book form
          render :new 
       end 
    end 
 
    def edit 
-      @book = Book.find_by_id(params[:id])
+      set_book
    end 
 
    def update 
-      @book = Book.find_by_id(params[:id])
+      set_book
       @book.update(book_params)
-      # @review = Book.reviews.build(rating: params[:rating], comment: params[:comment])
-      # if book was updated successfully, send to book show page
+
       if @book.save 
          redirect_to book_path(@book)
       else  
-         # otherwise render the edit form 
          render :edit 
       end 
    end 
 
    def show
-      @book = Book.find_by_id(params[:id])
-      # binding.pry
+      set_book
    end 
 
    def destroy
-      @book = Book.find_by_id(params[:id]).destroy
+      delete_book
       redirect_to books_path 
    end 
 
@@ -55,6 +50,20 @@ class BooksController < ApplicationController
    private
    def book_params
       params.require(:book).permit(
-         :title, :author, :description, :release_date, :category_name, :reviews_attributes => [:id, :rating, :comment])
+         :title, 
+         :author, 
+         :description, 
+         :release_date, 
+         :category_name, 
+         :reviews_attributes => [:id, :rating, :comment]
+      )
+   end 
+
+   def set_book
+      @book = Book.find_by_id(params[:id])
+   end
+
+   def delete_book
+      @book = Book.find_by_id(params[:id]).destroy
    end 
 end
