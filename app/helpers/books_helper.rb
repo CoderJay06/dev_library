15d6d7b9d6e4f59d7461c55f6e1786416de9b696
github username <github email address>
2020-09-02 00:30:24 -0400

    Refactor books index using helper and partial

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,10 @@
 module BooksHelper
+   def display_books_heading(book)
+      if @books == Book.recently_added 
+         "Recently added books"
+      else 
+         "Books Index"
+      end 
+   end 
+
 end
