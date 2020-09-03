4c8fc8c656b6f11e8f4aa63d9726b089f0768fa8
github username <github email address>
2020-09-03 17:24:34 -0400

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   root 'sessions#home'
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/home', to: 'sessions#home'
   get'/logout', to: 'sessions#destroy'
 
-  
   # Routes for google oauth
   get '/auth/:provider/callback', to: 'sessions#googleAuth'
   get 'auth/failure', to: redirect('/')
@@ -15,16 +12,15 @@ Rails.application.routes.draw do
   get '/users/:id/delete', to: 'users#delete'
   delete '/users/:id', to: 'users#destroy', as: 'delete_account'
   get '/users/:id/reviews', to: 'reviews#show', as: 'user_reviews'
-  delete '/books/:id/reviews/:id', to: 'reviews#destroy', as: 'delete_review'
-  post '/books/:id/downloads/:id', to: 'downloads#create' 
+  post '/books/:id/downloads/:id', to: 'downloads#create' , as: 'download_book'
   get '/books/recently_added', to: 'books#recently_added'
 
   resources :books do 
-    resources :reviews, except: [:destroy]
+    resources :reviews
     resources :downloads
   end
   resources :books
-  resources :users, except: [:destroy]
+  resources :users
   resources :categories, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
