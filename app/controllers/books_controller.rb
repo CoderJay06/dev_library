279ea375fa626ef3245bdb3e55ca04fc19828173
CoderJay06 <jaycru06@yahoo.com>
2020-09-10 23:06:56 -0400

    Delete commented out code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,6 @@ class BooksController < ApplicationController
    def show
       # Set array of download format types
       @download_formats = ["PDF", "EPUB", "DOC"]
-      # download = Download.find_by(params[:id])
-      # binding.pry
-      # download.format = params[:download][:format]
    end 
 
    def recently_added
