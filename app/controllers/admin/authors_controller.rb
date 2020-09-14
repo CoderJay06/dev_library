4c9839185bba9d031cb1ac2408dd7da4b7b1b323
CoderJay06 <jaycru06@yahoo.com>
2020-09-14 03:20:43 -0400

    Build out the functionality for updating an author

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -1,11 +1,11 @@
 class Admin::AuthorsController < ApplicationController
+   before_action :set_author, except: [:new, :delete]
    def new
       # render form to add a new author
       @author = Author.new
    end
  
    def create
-      # binding.pry
       # add and persist new author to the db
       @author = Author.create(author_params)
       if @author.save
@@ -17,12 +17,23 @@ class Admin::AuthorsController < ApplicationController
       end 
    end
 
+   def edit
+   end
+
+   def update
+      if @author.update(author_params)
+         @author.save
+         redirect_to author_path(@author)
+      else
+         render :edit 
+      end 
+   end 
+
    def delete
    end 
 
    def destroy
       # delete a specific author with their books
-      author = Author.find_by_id(params[:id])
       author.books.select {|book| book.destroy} 
       author.destroy
       redirect_to authors_path 
@@ -32,4 +43,8 @@ class Admin::AuthorsController < ApplicationController
    def author_params
       params.require(:author).permit(:name)
    end
+
+   def set_author 
+      @author = Author.find_by_id(params[:id])
+   end 
 end
